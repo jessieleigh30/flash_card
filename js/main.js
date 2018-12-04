0db299811bb7136d295bbd5f92231f4eb08ffb13
Jessica Anderson <jessie.leigh30@gmail.com>
2018-12-03 17:08:58 -0700

    cards made

--- js/main.js
+++ js/main.js
@@ -1,11 +1,31 @@
+document.addEventListener('DOMContentLoaded', () => {
 
-///multiple cards empty array let cards = 
-//add card
-//edit card
-//delete card-front and back 
-//front of card
-///back of card
+  
+  let overlay = document.querySelector('.overlay');
+  //add new card button
+  let addCard = document.querySelector('.add-card');
+  let closeMenu = document.querySelector('.close-menu');
+  let frontCard = document.querySelector('#frontcard');
+  let backCard = document.querySelector('#backcard');
+  let cardForm = document.querySelector('.new-card-form');
 
-var card = ["info"]
-var newCard;
+  let userFrontCard;
+  let userBackCard;
 
+
+  addCard.addEventListener('click', () => {
+    overlay.style.display = "flex";
+    closeMenu.addEventListener('click', () => {
+      overlay.style.display = "none";
+    });
+  });
+  cardForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    console.log("this works");
+    userFrontCard = frontCard.value;
+    userBackCard = backCard.value;
+    console.log(frontCard.value);
+    console.log(backCard.value);
+    console.log(userFrontCard, userBackCard)
+  });
+});
