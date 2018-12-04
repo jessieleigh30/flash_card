733cbdf403df11b988f59692622fef9d07eeccf1
Jessica Anderson <jessie.leigh30@gmail.com>
2018-12-03 20:26:27 -0700

    submit button

--- js/main.js
+++ js/main.js
@@ -5,7 +5,10 @@ document.addEventListener('DOMContentLoaded', () => {
   //add new card button
   let addCard = document.querySelector('.add-card');
   let closeMenu = document.querySelector('.close-menu');
+
+  //front card
   let frontCard = document.querySelector('#frontcard');
+  //back card
   let backCard = document.querySelector('#backcard');
   let cardForm = document.querySelector('.new-card-form');
 
@@ -21,11 +24,26 @@ document.addEventListener('DOMContentLoaded', () => {
   });
   cardForm.addEventListener('submit', (event) => {
     event.preventDefault();
-    console.log("this works");
-    userFrontCard = frontCard.value;
-    userBackCard = backCard.value;
-    console.log(frontCard.value);
-    console.log(backCard.value);
-    console.log(userFrontCard, userBackCard)
+    //console.log("this works");
+    // userFrontCard = frontCard.value;
+    // userBackCard = backCard.value;
+    // console.log(frontCard.value);
+    // console.log(backCard.value);
+    // console.log(userFrontCard, userBackCard)
+    //push this card to an array
+    // let card = {
+    //   front: frontCard.value,
+    //   back: backCard.value,
+    // }
+    // cardArray.push(card);
+    // frontCard.innerHTML = "";
+    // console.log(cardArray);
+    // displayCards();
+
+    // //function to display cards
+    // function displayCards() {
+    //   cardContainer.innerHTML = "";
+    // }
+
   });
 });
